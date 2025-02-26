e7a8190ddc42b46c3f80ff6d3b2168196b85b89a
Hydra1903 <duongquochy2019@gmail.com>
2025-02-27 00:39:43 +0700

    Update

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class Controller : MonoBehaviour
+{
+    public enum PlayerState
+    {
+        Idle,
+        Run,
+        Dead,
+        TakingDamage,
+        Spawn,
+        Tele
+    }
+
+    private PlayerState currentState;
+    private Animator animator;
+    private CharacterController controller;
+    public float moveSpeed = 5f;
+    public float diagonalMoveSpeed;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        currentState = PlayerState.Idle;
+
+        diagonalMoveSpeed = moveSpeed / 1.41f;
+    }
+
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        Debug.Log(moveX + " " + moveZ);
+        HandleState(moveX,moveZ);
+    }
+    public void ChangeState(PlayerState newState)
+    {
+        currentState = newState;
+    }
+    public void HandleState(float moveX,float moveZ)
+    {
+        switch (currentState)
+        {
+            case PlayerState.Idle:
+                animator.Play("Idle");
+                if (moveX != 0 || moveZ != 0)
+                {
+                    currentState = PlayerState.Run;
+                }
+                break;
+            case PlayerState.Run:
+                if (moveX != 0 && moveZ != 0)
+                {
+                    Vector3 move = transform.right * moveX + transform.forward * moveZ;
+                    controller.Move(move * diagonalMoveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    Vector3 move = transform.right * moveX + transform.forward * moveZ;
+                    controller.Move(move * moveSpeed * Time.deltaTime);
+                }
+                if (moveX > 0 && moveZ > 0)
+                {
+                    animator.Play("RunNE");
+                }
+                else if (moveX > 0 && moveZ < 0)
+                {
+                    animator.Play("RunSE");
+                }
+                                else if (moveX < 0 && moveZ < 0)
+                {
+                    animator.Play("RunSW");
+                }
+                else if (moveX < 0 && moveZ > 0)
+                {
+                    animator.Play("RunNW");
+                }
+                else if (moveX > 0)
+                {
+                    animator.Play("RunE");
+                }
+                else if (moveZ > 0)
+                { 
+                    animator.Play("RunN");
+                }
+                else if (moveX < 0)
+                {
+                    animator.Play("RunW");
+                }
+                else if (moveZ < 0)
+                {
+                    animator.Play("RunS");
+                }
+                else
+                {
+                    currentState = PlayerState.Idle;
+                }
+                break;
+        } 
+    }
+}
